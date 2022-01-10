cc54fb29a3898fb4fa70be5ae93e5f24ae62c34c
Michael Fuller <72899417+mike00fuller@users.noreply.github.com>
2022-01-10 09:49:52 -0500

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,7 +20,7 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy; Copyright 2017 Michael Fuller</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>Version Control - <a title="GitHub" href="http://github.com/mike00fuller/fullerupp">git</a></li>
            </ul>
 
         </div>
